6ab7e2992b0db46a08b72ec32a40e2a25f74093b
ninaschramm <schrammnina@outlook.com>
2023-06-25 17:56:17 -0300

    feat: saves the token on localstorage for persistance

--- src/store/store.js
+++ src/store/store.js
@@ -3,18 +3,24 @@ import { createStore } from 'vuex';
 const customStore = createStore({
   state: {
     user: null,
-    token: null,
+    token: localStorage.getItem('token') || null,
   },
   mutations: {
     setToken(state, token) {
       state.token = token;
+      localStorage.setItem('token', token); // Save token in localStorage
+    },
+    clearToken(state) {
+      state.token = null;
+      localStorage.removeItem('token'); // Remove token from localStorage
     },
   },
   actions: {},
-  getters: {},
+  getters: {
     isLoggedIn(state) {
       return !!state.token;
     },
+  },
 });
 
 export default customStore;
